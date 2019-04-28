4ca65ee15d525f5bfdc8dcaea1e5ee9bca2c4ca3
avalon <yuanqi.shan@aalto.fi>
2019-04-28 03:58:21 +0300

    Interactable glow animation

---
+++ Assets/Scripts/ScaleAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAnimator : MonoBehaviour
+{
+    public float minScale = 1.0f, maxScale = 2.0f, speed = 5.0f;
+    private float scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = Random.Range(minScale, maxScale);
+        transform.localScale = Vector3.one * scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scale += speed * Time.deltaTime;
+        if(scale > maxScale)
+        {
+            scale = maxScale;
+            speed *= -1;
+        } else if(scale < minScale) {
+            scale = minScale;
+            speed *= -1;
+        }
+        transform.localScale = Vector3.one * scale;
+    }
+}
